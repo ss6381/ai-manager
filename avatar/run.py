8fb28c7f9aca7047c61a6712e0a337b05b2f4264
Sparsh Srivastava <ss6381@columbia.edu>
2024-10-13 18:59:50 -0700

    remove secrets

--- avatar/run.py
+++ avatar/run.py
@@ -7,6 +7,7 @@ from llama_index.core.node_parser import SimpleNodeParser
 from pydantic import BaseModel
 
 import outspeed as sp
+import json
 
 nest_asyncio.apply()
 
@@ -59,47 +60,42 @@ class SearchTool(sp.Tool):
         logging.info(f"RAG Response: {response}")
         return SearchResult(result=str(response))
 
-
 @sp.App()
 class VoiceBot:
     async def setup(self) -> None:
-        # download and install dependencies
-        documents = SimpleDirectoryReader(f"{PARENT_DIR}/data/").load_data()
-        node_parser = SimpleNodeParser.from_defaults(chunk_size=512)
-        nodes = node_parser.get_nodes_from_documents(documents=documents)
-        vector_index = VectorStoreIndex(nodes)
-        self.query_engine = vector_index.as_query_engine(similarity_top_k=2)
         # Initialize the AI services
-        self.llm_node = sp.OpenAIRealtime(
-            tools=[
-                SearchTool(
-                    name="search",
-                    description="Search the web for information",
-                    parameters_type=Query,
-                    response_type=SearchResult,
-                    query_engine=self.query_engine,
-                )
-            ]
+        self.deepgram_node = sp.DeepgramSTT(sample_rate=8000)
+        self.llm_node = sp.GroqLLM(
+            system_prompt="You are a helpful assistant. Keep your answers very short. No special characters in responses.",
+        )
+        self.token_aggregator_node = sp.TokenAggregator()
+        self.tts_node = sp.CartesiaTTS(
+            voice_id="95856005-0332-41b0-935f-352e296aa0df",
+            api_key=os.getenv("CARTESIA_API_KEY")  # Ensure API key is loaded from environment
         )
 
     @sp.streaming_endpoint()
-    async def run(self, audio_input_queue: sp.AudioStream, text_input_queue: sp.TextStream):
-        # Set up the AI service pipeline
-        audio_output_stream: sp.AudioStream
-        audio_output_stream, text_output_stream = self.llm_node.run(text_input_queue, audio_input_queue)
+    async def run(self, audio_input_queue: sp.AudioStream, text_input_queue: sp.TextStream) -> sp.AudioStream:
+        deepgram_stream: sp.TextStream = self.deepgram_node.run(audio_input_queue)
 
-        return audio_output_stream, text_output_stream
+        text_input_queue = sp.map(text_input_queue, lambda x: json.loads(x).get("content"))
+
+        llm_input_queue: sp.TextStream = sp.merge(
+            [deepgram_stream, text_input_queue],
+        )
+
+        llm_token_stream, chat_history_stream = self.llm_node.run(llm_input_queue)
+
+        token_aggregator_stream: sp.TextStream = self.token_aggregator_node.run(llm_token_stream)
+        tts_stream: sp.AudioStream = self.tts_node.run(token_aggregator_stream)
+
+        return tts_stream
 
     async def teardown(self) -> None:
-        """
-        Clean up resources when the VoiceBot is shutting down.
-
-        This method is called when the app stops or is shut down unexpectedly.
-        It should be used to release resources and perform any necessary cleanup.
-        """
+        await self.deepgram_node.close()
         await self.llm_node.close()
-
+        await self.token_aggregator_node.close()
+        await self.tts_node.close()
 
 if __name__ == "__main__":
-    # Start the VoiceBot when the script is run directly
     VoiceBot().start()
